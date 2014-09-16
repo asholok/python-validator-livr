f124e55eefcfefc9d793f15024107a9fa79d5c5f
asholok <asholok@gmail.com>
2014-09-16 15:55:35 +0300

    change README

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-with open('README.rst') as readme:
+with open('README.txt') as readme:
     setup(
         name = 'LIVR',
         version = '0.4',
         author = 'Viktor Turstkiy, Ihor Kolosha',
         packages = ['LIVR', 'LIVR.test'],
-        license='LICENSE.txt',
+        license='look into README',
         description='LIVR validator.',
         long_description=readme.read(),
     )
